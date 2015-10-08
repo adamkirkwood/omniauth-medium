1bb10b7cc757d5b7ec50adf22c055e185074a696
Adam Kirkwood <hello@adamkirkwood.com>
2015-10-07 23:14:32 -0700

    initial commit

---
+++ lib/omniauth/medium.rb
@@ -0,0 +1,7 @@
+require "omniauth/medium/version"
+
+module Omniauth
+  module Medium
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/medium/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Medium
+    VERSION = "0.1.0"
+  end
+end
