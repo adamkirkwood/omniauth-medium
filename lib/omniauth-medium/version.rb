e1ea1cb8cbfa0748b09e4175cc91a94962317b75
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-03-18 09:48:15 +0000

    Add callback_url - Fixes regression in omniauth-oauth2

--- lib/omniauth-medium/version.rb
+++ lib/omniauth-medium/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module Medium
-    VERSION = '0.1.1'
+    VERSION = '0.1.1'.freeze
   end
 end
--- lib/omniauth/strategies/medium.rb
+++ lib/omniauth/strategies/medium.rb
@@ -2,19 +2,25 @@ require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
+    # Oauth2 strategy for medium.com
     class Medium < OmniAuth::Strategies::OAuth2
       option :name, 'medium'
-      option :scope, 'basicProfile,publishPost'
+      option :scope, 'basicProfile,listPublications'
 
-      option :client_options, {
-        site: 'https://api.medium.com/v1',
-        authorize_url: 'https://medium.com/m/oauth/authorize',
-        token_url: "https://api.medium.com/v1/tokens"
-      }
+      option :client_options,
+             site: 'https://api.medium.com/v1',
+             authorize_url: 'https://medium.com/m/oauth/authorize',
+             token_url: 'https://api.medium.com/v1/tokens'
 
-      uid { info["id"] }
+      uid { info['id'] }
 
-      info { @info ||= access_token.get("me").parsed["data"] }
+      info { @info ||= access_token.get('me').parsed['data'] }
+
+      def callback_url
+        # Fixes regression in omniauth-oauth2 v1.4.0 by
+        # https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+        options[:callback_url] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
